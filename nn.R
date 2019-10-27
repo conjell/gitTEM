cf68ae3a21b7c18c1781e60205e6cc77c28667c6
conjell <43294186+conjell@users.noreply.github.com>
2019-10-28 00:34:46 +0100

    Add neural network code (model refinement)
    Add neural network code (model refinement)

--- nn.R
+++ nn.R
@@ -1,4 +1,5 @@
 #install.packages("brnn")
+#document location
 setwd("/Users/kangzhao/Downloads/")
 #setwd("~/Desktop/NN")
 library('plyr')
@@ -7,23 +8,26 @@ library('brnn')
 library("caret")
 
 #train and test
-raw_data <- read.csv("without heating.csv", header=T)
+setwd("/Users/kangzhao/Downloads/")
+raw_data <- read.csv("with.csv", header=T)
+data <- raw_data[,c(2,12,15,16,17,18,19,20,21,22,23)]
+data <-data[complete.cases(data[,2]),]
+data<-data[data$X.1%in% c('1:00:00','2:00:00','3:00:00','4:00:00','5:00:00','6:00:00'), ]
+data<-data[data$X.1%in% c('7:00:00','8:00:00','9:00:00','10:00:00','11:00:00','12:00:00'), ]
+data<-data[data$X.1%in% c('13:00:00','14:00:00','15:00:00','16:00:00','17:00:00','18:00:00'), ]
+data<-data[data$X.1%in% c('19:00:00','20:00:00','21:00:00','22:00:00','23:00:00','0:00:00'), ]
+
+
 #raw 1-15: temperature data of sensor 1-15; raw 16-22: outdoor weather data
-data <- raw_data[,c(1,16,17,18,19,20,21,22)]
-data <-data[complete.cases(data[,1]),]
-split <-createDataPartition(y=data$SENSOR_1, p=0.8, list=FALSE)
+n=17
+split <-createDataPartition(y=data$SEN21, p=0.8, list=FALSE)
 train <- data[split, ]
 test <- data[-split, ]
 #neurons=number of hidden layers
-nn <- brnn(SENSOR_1 ~ TEMP+WINS+DEWP+PREA+AIRP+CLOC+HUM,neurons=7,train)
-p<-predict(nn,test)
-t<-test$SENSOR_25
-mse(t,p)
-rmse(t,p)
-relation<-lm(p~t)
-print(summary(relation))
-nn
-
+nw21.17.5 <- brnn(SEN21 ~ TEM+WIN+AIR+CLO+HUM+RAI,neurons=n,train)
+p<-predict(nw21.17.5,test)
+t<-test$SEN21
+rmse(t,p) 
 
 #k-cross validatation
 cv.error <- NULL
@@ -32,13 +36,13 @@ for(i in 1:k){
   index <- sample(1:nrow(data),round(0.8*nrow(data)))
   train.cv <- data[index,]
   test.cv <- data[-index,]
-  nn<-brnn(SENSOR_1 ~ TEMP+WINS+DEWP+PREA+AIRP+CLOC+HUM,neurons=7,data=train.cv)
+  nn<-brnn(SEN21 ~ TEM+WIN+AIR+CLO+HUM+RAI,neurons=n,data=train.cv)
   pr.nn<-predict(nn,test.cv)
-  test.cv.r <- test.cv$SENSOR_1
-  cv.error[i] <- mse(pr.nn,test.cv.r)
+  test.cv.r <- test.cv$SEN21
+  cv.error[i] <- rmse(pr.nn,test.cv.r)
 }
 mean(cv.error)
 cv.error
-boxplot(cv.error,xlab='MSE CV',col='cyan',
-        border='blue',names='CV error (MSE)',horizontal=TRUE)
+boxplot(cv.error,xlab='RMSE CV',col='cyan',
+        border='blue',names='CV error (RMSE)',horizontal=TRUE)
 
